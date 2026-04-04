cd7a2a297fdccf38f6378c27245a377aa6a95d96
RogerXSandip <rogerxsandip@RogerXSandips-MacBook-Air.local>
2026-04-05 04:09:15 +0530

    fix: remove spring boot projects, keep only config properties

--- adminService/src/main/java/com/amazing/technical/adminservice/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.amazing.technical.adminservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-	public ResourceNotFoundException(String message) {
-		super(message);
-	}
-}
